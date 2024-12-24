78a880be4e9bffe3a44fbf9ae3d995a868ef8eef
Mishra-capgemini <amit.kumar-mishra@capgemini.com>
2024-12-24 18:17:32 +0530

    Update BlogPage.java day 7

--- src/test/java/Pages/BlogPage.java
+++ src/test/java/Pages/BlogPage.java
@@ -1,11 +1,15 @@
 package Pages;
 
+import java.time.Duration;
 import java.util.Set;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BlogPage {
 	protected WebDriver driver;
@@ -63,7 +67,7 @@ public class BlogPage {
 	public String validatePopUpForSubscribe() {
 		// TODO Auto-generated method stub
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -122,6 +126,12 @@ public class BlogPage {
 	
 	public void subscribeNowButton() {
 		// TODO Auto-generated method stub
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		WebElement subcribeNow=driver.findElement(By.id("submit-btn"));
 		subcribeNow.click();
 		
@@ -133,7 +143,7 @@ public class BlogPage {
 		// TODO Auto-generated method stub
 		//System.out.println("i will verify it");
 		try {
-			Thread.sleep(7000);
+			Thread.sleep(6000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -142,7 +152,7 @@ public class BlogPage {
 		//String text=wait.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class='submitMsg']//p[@class='submitMsg_heading' and text()=\"You're all set!\"]"))).getText();
 		System.out.println(text);
 		try {
-			Thread.sleep(5000);
+			Thread.sleep(7000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -172,20 +182,81 @@ public class BlogPage {
 
 	public String gotalertmsg() {
 		// TODO Auto-generated method stub
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		Alert alert;
 		alert=driver.switchTo().alert();
 		
 		 String text=alert.getText();
 		 System.out.println(text);
+		 try {
+				Thread.sleep(6000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		return text;
 		
 	}
+	
+	public void clickForwardButton() {
+		// TODO Auto-generated method stub
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 300)");
+        try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		WebElement forwardButton=driver.findElement(By.xpath("/html[1]/body[1]/main[1]/section[1]/i[2]/*[name()='svg'][1]/*[name()='g'][1]/*[name()='rect'][1]"));
+		forwardButton.click();
+		forwardButton.click();
+		forwardButton.click();
+		
+       
+	}
+	public void isNextImageDisplayed() {
+		// TODO Auto-generated method stub
+		//String text=driver.findElement(By.xpath("//p[text()='Saurabh Garg is the Founder and Chief Business officer at NoBroker']")).getText();
+		System.out.println("Imge is moving");
+		//return text;
+	}
+
+
+
+
+
+	public void clickBackwardButton() {
+		// TODO Auto-generated method stub
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//WebElement BackwardButton=driver.findElement(By.xpath("/html[1]/body[1]/main[1]/section[1]/i[1]/*[name()='svg'][1]/*[name()='g'][1]/*[name()='rect'][1]"));
+		WebElement BackwardButton=driver.findElement(By.xpath("//section[1]/i[1]/*[name()='svg'][1]/*[name()='g'][1]/*[name()='rect'][1]"));
+		BackwardButton.click();
+		BackwardButton.click();
+		
+		
+		
+		
+		
+	}
+
+
+
+
+
+	public void isPreviousImageDisplayed() {
+		// TODO Auto-generated method stub
+		//String text=driver.findElement(By.xpath("//p[text()='Amit Agrawal is the Founder and Chief Executive officer at NoBroker']")).getText();
+		System.out.println("Image is moving back");
+		
+	}
+
 
 
 	
